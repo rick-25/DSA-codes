e0d74d8ea994bfb3ac4b867546484686a2c85b32
Rick-c-137 <83104259+rick-25@users.noreply.github.com>
2021-04-24 22:17:12 +0530

    Create MedianOfTwoSortedArrays.java

---
+++ MedianOfTwoSortedArrays.java
@@ -0,0 +1,38 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        
+        if(nums1.length <= nums2.length)
+            return search(nums1, nums2, 0, nums1.length-1);
+        
+        return search(nums2, nums1, 0, nums2.length-1);
+    }
+    
+    int val(int[] a, int i) {
+        if(i < 0)           return Integer.MIN_VALUE;
+        if(i >= a.length)   return Integer.MAX_VALUE;
+        else                return a[i];
+    }
+
+
+    double search(int a[], int b[], int l, int r) {
+        
+        int x = l + (r-l)/2;
+        int y = ((a.length + b.length + 1) / 2) - x;
+
+
+        if(val(a, x-1) <= val(b, y) && val(b, y-1) <= val(a, x))  {
+            
+            if((a.length + b.length) % 2 == 0)
+                return (double) (Math.min(val(a, x), val(b, y)) + Math.max(val(a, x-1), val(b, y-1))) / 2;
+
+            return  Math.max(val(a, x-1), val(b, y-1));
+        }
+        else if(val(a, x-1) > val(b, y)) 
+            r = x-1;
+        else 
+            l = x+1;
+
+
+        return search(a, b, l, r);
+    }
+}
